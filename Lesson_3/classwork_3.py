d6d8adde1b83f8fd0ebacecf73857c1e29dc5747
Vit Haiduchyk <vit.haiduchyk@gmail.com>
2024-03-13 11:43:48 -0700

    changed code in classwork_3.py

--- Lesson_3/classwork_3.py
+++ Lesson_3/classwork_3.py
@@ -1,3 +1,18 @@
-print("test")
-print("new data")
-print("Test")
+age = 61
+
+if age > 18:
+    result = 'Middle age'
+else:
+    result = 'Not middle age'
+
+result = 'Middle age' if age > 18 else 'Not middle age'
+print(result)
+
+# if age >= 18:
+#     print("Adult person ")
+# elif age > 50:
+#     print("More than 50")
+# elif age == 3:
+#     print('Child')
+# else:
+#     print("Not adult person")
