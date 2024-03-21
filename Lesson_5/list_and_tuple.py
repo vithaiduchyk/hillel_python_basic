137543316a7d60bca3dd54f46386d9c970ebf09c
Vit Haiduchyk <vit.haiduchyk@gmail.com>
2024-03-21 15:51:16 -0700

    added lesson_5

---
+++ Lesson_5/list_and_tuple.py
@@ -0,0 +1,25 @@
+# names = ['Maryna', 'Ivan', 'Olha'] <class 'list'>
+
+# second = ['test', 'autotest']
+# names.extend(second)
+
+# age = [5, -674, 90, 1, 0, 10]
+# names.sort(key=str.upper)
+# print(names)
+
+# new_list = names.copy()
+# new_list.append('test')
+# print(names)
+# print(new_list)
+
+# test = sorted(names)
+# print(test)
+# print(names)
+
+names = 'Hillel IT school'
+new_tuple = tuple()     # <class 'tuple'>
+new_tuple_2 = 1, 2, 3, 4     # <class 'tuple'>
+
+print(names.split())
+
+
