1510a7056695d105443b59e6ccd888e7561512be
vit.haiduchyk <74875217+vithaiduchyk@users.noreply.github.com>
2024-03-23 13:26:23 -0700

    Update is_palindrome.py
    change_cleaned_string var

--- Homework_5/is_palindrome.py
+++ Homework_5/is_palindrome.py
@@ -3,7 +3,7 @@ input_string = input("Enter a string: ")
 
 # not sure about this method, it's what i found in stackoverflow
 # lower() - this ensures that the comparison for palindromes is case-insensitive.
-cleaned_string = ''.join(filter(str.isalnum, input_string.lower()))
+cleaned_string = input_string.lower()
 
 if cleaned_string == cleaned_string[::-1]:
     print(f"{input_string} is a palindrome!")
