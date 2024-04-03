53044beb5edc12d1a37a9f37b607544be4e96f59
vit.haiduchyk <74875217+vithaiduchyk@users.noreply.github.com>
2024-04-02 18:21:14 -0700

    Update Task_3.py
    optimized the code

--- Homework_7/Task_3.py
+++ Homework_7/Task_3.py
@@ -1,17 +1,6 @@
 numbers = input("Enter numbers by space: ").split()
 
-all_unique = True
-
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
-        if numbers[i] == numbers[j]:
-            all_unique = False
-            break
-    if not all_unique:
-        break
-
-if all_unique:
-    print("All numbers is unique")
+if len(numbers) == len(set(numbers)):
+    print(f'{numbers}' list is unique!)
 else:
-    print("Numbers not unique")
-
+    print(f'{numbers} list is not unique')
