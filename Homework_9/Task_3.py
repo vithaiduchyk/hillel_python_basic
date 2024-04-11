1031e3ab7060bf360832e0d557379e654ea710d8
vit.haiduchyk <74875217+vithaiduchyk@users.noreply.github.com>
2024-04-10 17:59:58 -0700

    Update Task_3.py
    fixed try

--- Homework_9/Task_3.py
+++ Homework_9/Task_3.py
@@ -2,8 +2,7 @@ course = {'title': 'AQA', 'language': 'Python', 'level': 'PRO', 'frame': 'pytest
 
 user_input = input("Enter key: ")
 try:
-    if user_input == 'title' or 'language' or 'level' or 'frame':
-        print(course[user_input])
+        print(f'Your result is {course[user_input]}')
 except KeyError:
     print(f'Wrong key name!')
 
