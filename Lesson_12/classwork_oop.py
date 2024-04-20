b027b2157b9d663db5114b8ecd7eaad49b812e84
Vit Haiduchyk <vit.haiduchyk@gmail.com>
2024-04-19 18:12:28 -0700

    Added second part of lesson 12

--- Lesson_12/classwork_oop.py
+++ Lesson_12/classwork_oop.py
@@ -1,7 +1,29 @@
 class Car:
 
+    def __init__(self, mark, age, model):
+        self.mark = mark
+        self.age = age
+        self.model = model
+
     def move(self):
-        return 'I go'
+        return f'{self.mark} is going'
+
+    def turn_left(self):
+        pass
+
+    def check_age(self):
+        if self.age < 10:
+            return 'Too old'
+        else:
+            return 'Good'
+
+    def go_straight(self):
+        pass
+
+    def go(self):
+        self.move()
+        self.go_straight()
+        self.turn_left()
 
 if __name__ == '__main__':
     bmw = Car()
